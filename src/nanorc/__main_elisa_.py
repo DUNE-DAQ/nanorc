e9ef920d792faa9c2b44df2d5f684a4bb7c7465c
Pierre Lasorak <pierre.lasorak@gmail.com>
2021-12-08 15:59:01 +0100

    a main for accessing only elisa

---
+++ src/nanorc/__main_elisa_.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+Command Line Interface for NanoRC
+"""
+
+from .cli import *
+
+def main():
+    from rich.logging import RichHandler
+
+    logging.basicConfig(
+        level="INFO",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
+
+    console = Console()
+    obj = NanoContext(console)
+
+    try:
+        cli(obj=obj, show_default=True)
+    except Exception as e:
+        console.log("[bold red]Exception caught[/bold red]")
+        if not obj.print_traceback:
+            console.log(e)
+        else:
+            console.print_exception()
+
+if __name__ == '__main__':
+    main()
+
