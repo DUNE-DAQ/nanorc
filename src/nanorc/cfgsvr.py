74b8ae22cef3e86b21b65d28cceda24ff025ea12
Pierre Lasorak <pierre.lasorak@gmail.com>
2021-12-16 18:08:08 +0100

    grab version from env

--- src/nanorc/cfgsvr.py
+++ src/nanorc/cfgsvr.py
@@ -194,11 +194,14 @@ class DBConfigSaver:
 
             with open(fname, "rb") as f:
                 files = {'file': f}
+                version = os.getenv("DUNEDAQ_RELEASE")
+                if not version:
+                    raise RuntimeError('RunRegistryDB: dunedaq version not in the env!')
+
                 post_data = {"run_num": run,
                              "det_id": self.apparatus_id,
                              "run_type": run_type,
-                             ## Big hack of the version!
-                             "software_version": "dunedaq-v2.8.2"}
+                             "software_version": version}
 
                 try:
                     r = requests.post(self.API_SOCKET+"/runregistry/insertRun/",
