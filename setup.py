70f7f689c4991339550d012bd752dbbf6e2b7818
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-03-16 17:54:58 +0100

    add needed package and export the data from template and static folder

--- setup.py
+++ setup.py
@@ -3,10 +3,26 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="nanorc",
+    package_data={
+        'nanorc': [
+            ## yes, I really hate this too
+            'webuidata/*',
+            'webuidata/*/*',
+            'webuidata/*/*/*',
+            'webuidata/*/*/*/*',
+            'webuidata/*/*/*/*/*',
+            'webuidata/*/*/*/*/*/*',
+            'webuidata/*/*/*/*/*/*/*',
+            'webuidata/*/*/*/*/*/*/*/*',
+            'confdata/*']
+    },
     install_requires=[
         "click",
         "click-shell",
         "Flask",
+        "Flask-HTTPAuth",
+        "Flask-RESTful",
+        "Flask-Cors",
         "rich",
         "sh",
         "graphviz",
