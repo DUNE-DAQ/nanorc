7e41c8b3185e25b84493a50f6d846625c7f4c9f2
Jonathan Hancock <jhancock0@protonmail.com>
2022-05-31 18:54:58 +0200

    Changed the partition test to work with the updated testing framework, without the need for shell scripting.

--- integtest/partition_test.py
+++ integtest/partition_test.py
@@ -9,8 +9,11 @@ confgen_name="daqconf_multiru_gen"
 # The arguments to pass to the config generator, excluding the json
 # output directory (the test framework handles that)
 confgen_arguments=[ "-o", ".", "-s", "10", "-n", "2"]
-# The commands to run in nanorc, as a list
-nanorc_command_list="boot init conf start 1 resume wait 10 stop scrap terminate".split()
+# The commands to run in nanorc, as a dictionary
+nanorc_keys = [("Partition No. " + str(i)) for i in range(10)]
+nanorc_script = "boot init conf start 1 resume wait 10 stop scrap terminate".split()
+nanorc_values = [["--partition-number", str(i)] + nanorc_script for i in range(10)]
+nanorc_command_list = {nanorc_keys[i]: nanorc_values[i] for i in range(10))}            #This variable is the one that gets sent to pytest
 
 # The tests themselves
 def test_nanorc_success(run_nanorc_custom):
