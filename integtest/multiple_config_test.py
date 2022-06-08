4f806da92a248b8ee50cdd7b2d2c5d921945565f
Jonathan Hancock <jhancock0@protonmail.com>
2022-06-08 18:26:32 +0200

    double_config_test renamed to multiple_config_test since you can use an arbitrary number of configs. Some changes to make it compatible with alterations to test framework: confgen_name can be given the value "multi" to activate the new features.

--- integtest/multiple_config_test.py
+++ integtest/multiple_config_test.py
@@ -4,11 +4,15 @@ import pytest
 # file. They're read by the "fixtures" in conftest.py to determine how
 # to run the config generation and nanorc
 
-# The name of the python module for the config generation
-confgen_name="daqconf_multiru_gen"
+# The name of the python module for the config generation.
+# Use "multi" if multiple configs per test are being used
+confgen_name="multi"
+
 # The arguments to pass to the config generator, excluding the json
-# output directory (the test framework handles that)
-confgen_arguments=[ "-o", ".", "-s", "10", "-n", "2"] #Need to figure out how to do two configs!
+# output directory (the test framework handles that).
+#When multiple configs are used per test, then start each one with the module used for config generation, and end each with "END".
+confgen_arguments=[ "daqconf_multiru_gen", "-o", ".", "-s", "10", "-n", "2", "END", "daqconf_multiru_gen", "-o", ".", "-s", "15", "-n", "2", "END"]
+
 # The commands to run in nanorc, as a list
 nanorc_command_list="boot init conf start 1 resume wait 10 stop scrap terminate".split()
 
