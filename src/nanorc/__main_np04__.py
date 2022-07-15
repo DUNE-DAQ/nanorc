37608b80f5c5edd3079b0f7893b2412392268509
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-07-15 12:03:57 +0200

    make nano04rc work with trigger_rate

--- src/nanorc/__main_np04__.py
+++ src/nanorc/__main_np04__.py
@@ -196,7 +196,7 @@ def add_run_start_parameters():
     # sigh start...
     def add_decorator(function):
         f1 = click.argument('run-type', required=True, type=click.Choice(['TEST', 'PROD']))(function)
-        f2 = click.option('--trigger-interval-ticks', type=int, default=None, help='Trigger separation in ticks')(f1)
+        f2 = click.option('--trigger-rate', type=float, default=None, help='Trigger rate in Hz')(f1)
         f3 = click.option('--disable-data-storage/--enable-data-storage', type=bool, default=False, help='Toggle data storage')(f2)
         f4 = accept_timeout(None)(f3)
         return click.option('--message', type=str, default="")(f4)
