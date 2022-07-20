865773cd66fe73d8dead4626ec5a52f159e6e3fe
Pierre Lasorak <pierre.lasorak@gmail.com>
2022-07-20 18:05:55 +0200

    change the uploading script

--- src/nanorc/tools/upload_conf.py
+++ src/nanorc/tools/upload_conf.py
@@ -75,28 +75,32 @@ def upload_conf(url, json_dir, name, verbose):
         data=json.dumps(conf_data)
     )
 
-    resp_data = response.json()
+    if response.status_code == 200:
+      resp_data = response.json()
 
-    if verbose:
-        console.log(f"conf service responded with {resp_data}")
-    if not resp_data['success']:
-        raise RuntimeError(f'Couldn\'t upload your configuration: {resp_data["error"]}')
+      if verbose:
+          console.log(f"conf service responded with {resp_data}")
+      if not resp_data['success']:
+          raise RuntimeError(f'Couldn\'t upload your configuration: {resp_data["error"]}')
 
-    docid=resp_data['docid']
-    version=resp_data['version']
-    coll_name=resp_data['coll_name']
-    console.log(f'Uploaded [blue]{json_dir}[/blue] to the configuration service (url: \'{url}\') with name: [blue]{name}[/blue], version: [blue]{version}[/blue]')
+      docid=resp_data['docid']
+      version=resp_data['version']
+      coll_name=resp_data['coll_name']
 
-    return docid, coll_name, version
+      console.log(f'Uploaded [blue]{json_dir}[/blue] to the configuration service (url: \'{url}\') with name: [blue]{name}[/blue], version: [blue]{version}[/blue]')
+
+      return docid, coll_name, version
+    else:
+      console.log(f'[red bold]Couldn\'t upload {json_dir} to the configuration service (url: \'{url}\') with name: {name}[/red bold]')
 
 
 def main():
-    try:
-        upload_conf()
-    except Exception as e:
-        console.log("[bold red]Exception caught[/bold red]")
-        console.log(e)
-        console.print_exception()
+  try:
+    upload_conf()
+  except Exception as e:
+    console.log("[bold red]Exception caught[/bold red]")
+    console.log(e)
+    console.print_exception()
 
 if __name__ == '__main__':
-    main()
+  main()
