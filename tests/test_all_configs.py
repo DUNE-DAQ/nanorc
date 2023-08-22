3f62f4b7a3cecf00dad362abb93fabf895cfe60b
Jonathan Hancock <jhancock0@protonmail.com>
2023-08-22 16:03:00 +0200

    Configs are now generated as part of the test, using daqconf_multiru_gen and a  particular hardware map. test_all_configs.py tests only a standard config and a top-json, as the database only works with k8s.

--- tests/test_all_configs.py
+++ tests/test_all_configs.py
@@ -4,23 +4,38 @@ import pytest
 import subprocess
 import tempfile
 
-conf_list = ["default-config", "top-level.json", r"db://jhancock-basic"]
+conf_types = ["normal", "top-json"]
 commands = "boot conf start_run 111 wait 60 stop_run scrap terminate".split()
 
-@pytest.fixture(params = conf_list)
+@pytest.fixture(params = conf_types)
 def perform_all_runs(request):
-    '''For each config in conf_list, run some commands with nanorc'''
+    '''
+    We generate a config using daqconf_multiru_gen, then run nanorc with it in three different ways.
+    The error code of the process is used to determine whether everything worked.
+    All processes are run in a temporary directory, so as not to fill up the CWD with logs.
+    '''
     start_dir = os.getcwd()
     temp_dir_object = tempfile.TemporaryDirectory()
-    temp_dir_name = temp_dir_object.name                #Make a temp directory and then enter it.
-    os.chdir(temp_dir_name)
-    if request.param[:5] == "db://":
-        conf_name = request.param
-        partition_name = f"test-partition-for-{conf_name[5:]}"
-    else:
-        conf_name = f"{start_dir}/{request.param}"      #We are somewhere in /tmp so the absolute path is needed.
-        partition_name = f"test-partition-for-{request.param.split('.')[0]}"
-    arglist = ["nanorc", "--loglevel", "CRITICAL", conf_name, partition_name] + commands
+    temp_dir_name = temp_dir_object.name                                        #Make a temp directory.
+    os.popen(f'cp {start_dir}/my_dro_map.json {temp_dir_name}/my_dro_map.json') #Copy the DRO map inside.
+    os.chdir(temp_dir_name)                                                     #Move into the temp dir.
+
+    conf_name = "test-conf"
+    db_name = "integ-test-conf"
+    DMG_args = ["daqconf_multiru_gen", "-m", "my_dro_map.json", conf_name]
+    subprocess.run(DMG_args)                                                    #Generate a config
+    partition_name = f"test-partition-{request.param}"
+
+    match request.param:
+        case "normal":
+            arglist = ["nanorc", conf_name, partition_name] + commands
+
+        case "top-json":        #Two duplicates of the regular config is enough to test that a top level json is functional
+            TJ_content = {"apparatus_id": "test", "sub1": conf_name, "sub2": conf_name}
+            with open("top-level.json", "w") as outfile:
+                json.dump(TJ_content, outfile)
+            arglist = ["nanorc", "top-level.json", partition_name] + commands
+
     output = subprocess.run(arglist)
     os.chdir(start_dir)
     return output.returncode
