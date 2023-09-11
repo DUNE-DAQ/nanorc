85c97c2fc92c80a8136e77e1778af7d0b4e10180
Jonathan Hancock <jhancock0@protonmail.com>
2023-09-11 16:11:48 +0200

    Expert command test now works for SSH and k8s process managers

--- tests/test_expert_commands.py
+++ tests/test_expert_commands.py
@@ -5,11 +5,12 @@ import subprocess
 import tempfile
 
 app_name = "trigger"
-
+k8s_json_contents = {"boot": {"use_connectivity_service": True, "connectivity_service_host": "np04-srv-023", "connectivity_service_port": 30005, "start_connectivity_service": False},
+                 "readout": {"use_fake_data_producers": True}}
 expert_json = {"id": "record", "entry_state": "ANY", "exit_state": "ANY", "data": {}}
-conf_types = ["normal"]
-commands = "boot expert_command expert.json test-conf/test-conf/dfo".split()
+conf_types = ["normal", "k8s"]
 conf_name = "test-conf"
+commands = f"boot expert_command {conf_name}/{conf_name}/dfo expert.json".split()
 cluster_address = "k8s://np04-srv-015:31000"
 
 @pytest.fixture(params = conf_types)
@@ -25,17 +26,23 @@ def perform_all_runs(request):
     os.popen(f'cp {start_dir}/my_dro_map.json {temp_dir_name}/my_dro_map.json') #Copy the DRO map inside.
     os.chdir(temp_dir_name)                                                     #Move into the temp dir.
 
-    DMG_args = ["daqconf_multiru_gen", "-m", "my_dro_map.json", conf_name]
-    subprocess.run(DMG_args)                                                    #Generate a config
-    partition_name = f"test-partition-{request.param}"
-    with open('expert.json', 'w') as json_file1:
-        json.dump(expert_json, json_file1)
-
     match request.param:
         case "normal":
+            DMG_args = ["daqconf_multiru_gen", "-m", "my_dro_map.json", conf_name]
+            subprocess.run(DMG_args)
+            partition_name = f"test-partition-{request.param}"
+            with open('expert.json', 'w') as json_file1:
+                json.dump(expert_json, json_file1)
             arglist = ["nanorc", conf_name, partition_name] + commands
 
         case "k8s":
+            with open('conf.json', 'w') as json_file:
+                json.dump(k8s_json_contents, json_file)
+            DMG_args_k8s = ["daqconf_multiru_gen", "--force", "-c", "conf.json", "-m", "my_dro_map.json", "--force-pm", "k8s", conf_name]
+            subprocess.run(DMG_args_k8s)
+            partition_name = f"test-partition-{request.param}"
+            with open('expert.json', 'w') as json_file1:
+                json.dump(expert_json, json_file1)
             arglist = ["nanorc", "--pm", cluster_address, conf_name, partition_name] + commands
 
     output = subprocess.run(arglist)
