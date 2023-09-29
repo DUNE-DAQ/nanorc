a3fb75dcc97a10dc53d8dcc6052fc40bb5a9f853
Pierre Lasorak <pierre.lasorak@gmail.com>
2023-09-29 16:01:18 +0200

    makes the test_all_configs sort of work

--- integtests/test_all_configs.py
+++ integtests/test_all_configs.py
@@ -3,13 +3,16 @@ import os
 import pytest
 import subprocess
 import tempfile
+from nanorc.integ_utils import get_default_config_dict, write_config, generate_dromap_contents, log_has_no_errors, logs_are_error_free, get_a_port_with_500_consecutive_ports_open
 
 conf_types = ["normal", "top-json"]
 exe_names = ["nanorc", "nanotimingrc"]
-cmd_dict = {"nanorc": "boot conf start_run 111 wait 60 stop_run scrap terminate".split(),
-            "nano04rc": "boot conf start_run TEST wait 60 stop_run scrap terminate".split(),
-            "nanotimingrc": "boot conf start_run wait 60 stop_run scrap terminate".split()
-           }
+#exe_names = ["nanotimingrc"]
+cmd_dict = {
+    "nanorc": "boot conf conf start_run 111 wait 5 stop_run scrap terminate".split(),
+    "nano04rc": "boot conf start_run TEST wait 5 stop_run scrap terminate".split(),
+    "nanotimingrc": "boot conf start_run wait 5 stop_run scrap terminate".split()
+}
 
 def perform_all_runs(exe_name, conf_type):
     '''
@@ -19,16 +22,49 @@ def perform_all_runs(exe_name, conf_type):
     '''
     commands = cmd_dict[exe_name]
     start_dir = os.getcwd()
-    temp_dir_object = tempfile.TemporaryDirectory()
-    temp_dir_name = temp_dir_object.name                                        #Make a temp directory.
-    os.popen(f'cp {start_dir}/my_dro_map.json {temp_dir_name}/my_dro_map.json') #Copy the DRO map inside.
-    os.chdir(temp_dir_name)                                                     #Move into the temp dir.
 
-    conf_name_1 = "test-conf"
-    conf_name_2 = "extra-test-conf"
-    DMG_args_1 = ["daqconf_multiru_gen", "-m", "my_dro_map.json", conf_name_1]
-    DMG_args_2 = ["daqconf_multiru_gen", "-m", "my_dro_map.json", conf_name_2]
-    subprocess.run(DMG_args_1)                                                  #Generate a config
+    temp_dir_object = tempfile.TemporaryDirectory()
+    temp_dir_name = temp_dir_object.name                                            # Make a temp directory.
+
+    dro_file_name = f'{temp_dir_name}/dro.json'
+    config_file_name_1 = f'{temp_dir_name}/conf1.json'
+    config_file_name_2 = f'{temp_dir_name}/conf2.json'
+
+    with open(dro_file_name, 'w') as f:
+        f.write(generate_dromap_contents(n_streams=2))
+
+
+    port1 = get_a_port_with_500_consecutive_ports_open()
+    config_data_1 = get_default_config_dict()
+    config_data_1["boot"]["base_command_port"] = port1+1
+    config_data_1["boot"]["connectivity_service_port"] = port1
+    config_data_1["detector"]["op_env"] = "nanorc-integtest"
+    config_data_1["daq_common"]["data_rate_slowdown_factor"] = 1
+    config_data_1["detector"]["clock_speed_hz"] = 62500000 # DuneWIB/WIBEth
+    config_data_1["readout"]["use_fake_cards"] = True
+    write_config(config_file_name_1, config_data_1)
+
+    from copy import deepcopy as dc
+    config_data_2 = dc(config_data_1)
+    config_data_2["boot"]["base_command_port"] = port1+1
+    config_data_2["boot"]["connectivity_service_port"] = port1
+
+
+    write_config(config_file_name_2, config_data_2)
+
+    conf_name_1 = f'{temp_dir_name}/test-conf-1'
+    conf_name_2 = f'{temp_dir_name}/test-conf-2'
+    top_level = f'{temp_dir_name}/top-level.json'
+
+    DMG_args_1 = ["fddaqconf_gen","-c", config_file_name_1, "-m", dro_file_name, conf_name_1]
+    DMG_args_2 = ["fddaqconf_gen","-c", config_file_name_2, "-m", dro_file_name, conf_name_2]
+
+    try:
+        subprocess.run(DMG_args_1)
+    except Exception as e:
+        pytest.fail(reason=str(e))
+
+
     partition_name = f"test-partition-{conf_type}"
 
     match conf_type:
@@ -37,11 +73,19 @@ def perform_all_runs(exe_name, conf_type):
 
         case "top-json":        #Two duplicates of the regular config is enough to test that a top level json is functional
             subprocess.run(DMG_args_2)
-            TJ_content = {"apparatus_id": "test", "sub1": conf_name_1, "sub2": conf_name_2}
-            with open("top-level.json", "w") as outfile:
-                json.dump(TJ_content, outfile)
-            arglist = [exe_name, "top-level.json", partition_name] + commands
 
+            TJ_content = {
+                "apparatus_id": "test",
+                "sub1": conf_name_1,
+                "sub2": conf_name_2
+            }
+
+            with open(top_level, "w") as outfile:
+                json.dump(TJ_content, outfile)
+
+            arglist = [exe_name, top_level, partition_name] + commands
+
+    os.chdir(temp_dir_name)
     output = subprocess.run(arglist)
     os.chdir(start_dir)
     return output.returncode
